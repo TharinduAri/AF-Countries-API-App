49eed637ab4d81ec2765b4af7fac03e889abffd5
TharinduAri <tharindulakshita2001@gmail.com>
2025-04-30 16:36:02 +0530

    frontend fixed

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,25 +1,23 @@
-const API_URL = 'af-countries-api-app-production.up.railway.app/api/users/';
+const API_URL = 'https://af-countries-api-app-production.up.railway.app/api/users/';
+import axios from 'axios';
 
 export const authService = {
+  // New account signup
   register: async (username: string, email: string, password: string): Promise<any> => {
-    const response = await fetch(API_URL + 'register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name: username, email, password }),
+    const response = await axios.post(`${API_URL}register`, {
+      name: username,
+      email,
+      password
     });
-    return await response.json();
+    return response.data;
   },
 
+  // Login
   login: async (email: string, password: string): Promise<any> => {
-    const response = await fetch(API_URL + 'login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
+    const response = await axios.post(`${API_URL}login`, {
+      email,
+      password
     });
-    return await response.json();
+    return response.data;
   },
 };
