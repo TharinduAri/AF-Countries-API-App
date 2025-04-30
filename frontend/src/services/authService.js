d6853c18ca88c4c82eba6aff20466ec6bc262572
TharinduAri <tharindulakshita2001@gmail.com>
2025-05-01 01:21:22 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = 'https://af-countries-api-app-production.up.railway.app/api/users/';
 
 export const authService = {
   register: async (username, email, password) => {
